0104b4cc3fdd112aa3d7e970ed477a3e4f26e16c
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-12-20 15:26:11 -0300

    Se trabaja sobre getall

--- controllers/productos.js
+++ controllers/productos.js
@@ -1,14 +1,14 @@
-const FS = require('fs');
 
 const ProductModel = require('../models/productos');
+const ProductosFile = new ProductModel('productos');
 
 function getById(req, res){
     console.log('Viewing ' + req.params.id);
 }
 
 function getAll(req, res){
-    res.send('All todos')
-    
+    ProductosFile.getAll().then((data) => res.send(`PRODUCTOS DISPONIBLES:  ${JSON.stringify(data)}`))
+                          .catch((err) => {throw err}); 
 }
 
 function save(req, res){
