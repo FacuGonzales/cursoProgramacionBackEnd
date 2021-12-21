205d7764939a10d79508e19d4b34185b7ebfeb5a
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-12-20 23:14:50 -0300

    Se modifican rutas

--- routes/productos.js
+++ routes/productos.js
@@ -3,10 +3,11 @@ const ProductosContoller = require('../controllers/productos');
 
 const api = EXPRESS.Router();
 
-api.get('/producto/:id', ProductosContoller.getById);
-api.get('/productos', ProductosContoller.getAll);
-api.post('/productos', ProductosContoller.save);
-api.put('/producto/:id', ProductosContoller.update);
-api.delete('/producto/:id', ProductosContoller.deleteProduct);
+
+api.get('/obtenerPorId/:id', ProductosContoller.getById);
+api.get('/obtenerProductos', ProductosContoller.getAll);
+api.post('/cargarProductos', ProductosContoller.save);
+api.put('/actualizarProducto/:id', ProductosContoller.update);
+api.delete('/eliminarProducto/:id', ProductosContoller.deleteProduct);
 
 module.exports = api;
