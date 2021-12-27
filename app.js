761f0f0f2c1dd1e05b03f3d475b32317d3da9cc1
fGonzalesLiendo <facundogonzalesliendo@gmail.com>
2021-12-27 14:36:14 -0300

    Se declaran los templates que se utilizan

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const EXPRESS = require('express');
 const app = EXPRESS();
+// const handlebars = require('express-handlebars')
 
 const ProductRoutes = require('./routes/productos');
+
+//-------------- UTILIZANDO HANDLEBARS ------------------------------
+// app.engine(
+//     "hbs",
+//     handlebars({
+//         extname: ".hbs",
+//         defaultLayout: 'index.hbs',
+//     })
+// );
+// app.set("view engine", "hbs");
+// app.set("views", "./views/hbs-templates");
+
+//-------------- UTILIZANDO PUG ------------------------------
+// app.set('views', './views/pug-template');
+// app.set('view engine', 'pug');
+
+//-------------- UTILIZANDO EJS ------------------------------
+app.set('views', './views/ejs-templates');
+app.set('view engine', 'ejs');
 
 app.use(EXPRESS.urlencoded({extended: true}))
 app.use(EXPRESS.static('public'))
